6977f10a3656872950e9af32200f6c1b56ff7120
Dilshodbek Abdullayev <abdullayevdilshodbek1998@gmail.com>
2024-05-15 02:57:54 +0500

    adding Dependency Injections

---
+++ MassaMaster.Infrastructure/InfrastructureDependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MassaMaster.Infrastructure
+{
+    public static class InfrastructureDependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<IRumassaDbContext, RumassaDbContext>(options =>
+                options
+                    .UseLazyLoadingProxies()
+                        .UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+            return services;
+        }
+    }
+}
--- MassaMaster.Infrastructure/Persistance/MassaMasterDbContext.cs
+++ MassaMaster.Infrastructure/Persistance/MassaMasterDbContext.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using MassaMaster.Domain.Entities;
+using MassaMaster.Domain.Entities.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MassaMaster.Domain.Entities.Auth;
 
 namespace MassaMaster.Infrastructure.Persistance
 {
@@ -15,7 +18,29 @@ namespace MassaMaster.Infrastructure.Persistance
         {
             Database.Migrate();
         }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Coupon> Coupons { get; set; }
+        public DbSet<Delivery> Deliveries { get; set; }
+        public DbSet<News> News { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<User> Users { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
 
-        
+            modelBuilder.Entity<Category>()
+                .HasData(
+                    [   new Category{ Id = 1, Name = "Оральные препараты"  },
+                        new Category{ Id = 2, Name = "Инъекционные препараты" },
+                        new Category{ Id = 3, Name = "Препараты ПКТ" },
+                        new Category{ Id = 4, Name = "Гормон роста" },
+                        new Category{ Id = 5, Name = "Сармы/Sarms" }
+                    ]
+                );
+        }
+
     }
 }
