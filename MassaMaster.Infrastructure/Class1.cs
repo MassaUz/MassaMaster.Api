2a0dc12d555abdb54116ed315a884d7543faba83
Dilshodbek Abdullayev <abdullayevdilshodbek1998@gmail.com>
2024-05-15 01:34:31 +0500

    Adding LoginDTOs

--- MassaMaster.Infrastructure/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MassaMaster.Infrastructure
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ MassaMaster.Infrastructure/Persistance/MassaMasterDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MassaMaster.Infrastructure.Persistance
+{
+    public class MassaMasterDbContext : IdentityDbContext<User,IdentityRole<Guid>,Guid> ,IMassaMasterDbContext
+    {
+        public MassaMasterDbContext(DbContextOptions<MassaMasterDbContext> options) : base(options) 
+        {
+            Database.Migrate();
+        }
+
+        
+    }
+}
