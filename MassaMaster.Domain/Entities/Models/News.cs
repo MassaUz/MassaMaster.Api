79cbbcdf7e1c76e6cc8cf06d4a4feded12ab720d
Dilshodbek Abdullayev <abdullayevdilshodbek1998@gmail.com>
2024-05-15 02:39:18 +0500

    adding News Model

---
+++ MassaMaster.Domain/Entities/Models/News.cs
@@ -0,0 +1,22 @@
+﻿using MassaMaster.Domain.Entities.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MassaMaster.Domain.Entities.Models
+{
+    public class News
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string CardPhotoPath { get; set; }
+        public string Date { get; set; }
+        public string Description { get; set; }
+        public Guid? UserId { get; set; }
+        public virtual User? User { get; set; }
+        public virtual List<Product> Products { get; set; }
+
+    }
+}
