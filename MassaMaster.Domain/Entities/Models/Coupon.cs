699e02ce800bdd969a01d204c7b6b4faf270c49c
Dilshodbek Abdullayev <abdullayevdilshodbek1998@gmail.com>
2024-05-15 02:37:47 +0500

    adding Coupon Model

---
+++ MassaMaster.Domain/Entities/Models/Coupon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MassaMaster.Domain.Entities.Models
+{
+    public class Coupon
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public DateTimeOffset ExpireDate { get; set; }
+        public int Limit { get; set; } = 2;
+        public short Percent { get; set; } = 10;
+    }
+}
