97a4db99cb34f4fc409c7ee68de0314adaa9b169
Dilshodbek Abdullayev <abdullayevdilshodbek1998@gmail.com>
2024-05-15 02:43:13 +0500

    Model And DTOs finished

---
+++ MassaMaster.Domain/Entities/Models/Review.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MassaMaster.Domain.Entities.Models
+{
+    public class Review
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Text { get; set; }
+        public Guid? ProductId { get; set; }
+        public virtual Product? Product { get; set; }
+    }
+}
