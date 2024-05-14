441ded4f41b7db547f466feccaf55879b7a13564
Dilshodbek Abdullayev <abdullayevdilshodbek1998@gmail.com>
2024-05-15 02:31:10 +0500

    adding Comment model

---
+++ MassaMaster.Domain/Entities/Auth/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MassaMaster.Domain.Entities.Auth
+{
+    public class User
+    {
+
+    }
+}
